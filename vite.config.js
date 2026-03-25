e25f8f583ef4e41d839727e41e3d6c9dead490ac
re111um <qkrcksgud47@gmail.com>
2026-03-25 13:56:00 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 이 부분이 './'로 되어 있어야 경로 오류가 나지 않습니다.
 })
