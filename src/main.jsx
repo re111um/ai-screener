f0ff202f053806c914c3bfaeeaf7391c2f93a14a
re111um <qkrcksgud47@gmail.com>
2026-03-25 13:53:12 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // App.jsx 파일을 불러옴
+import './index.css' // 만약 index.css가 없다면 이 줄은 지워도 됩니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
